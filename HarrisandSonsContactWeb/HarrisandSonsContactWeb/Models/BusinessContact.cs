01377e59d1306819529d8c3f03440e20e6f0a65f
SucileaMarcus <sucileamarcus@gmail.com>
2020-04-16 14:07:52 +0000

    Controllers Commit
    Implemented Controllers

---
+++ HarrisandSonsContactWeb/HarrisandSonsContactWeb/Models/BusinessContact.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HarrisandSonsContactWeb.Models
+{
+    public class BusinessContact
+    {
+        public int BusinessContactID { get; set; }
+        public string BusinessFname { get; set; }
+        public string BusinessLname { get; set; }
+        public string BusinessEmail { get; set; }
+        public string BusinessAddress1 { get; set; }
+        public string BusinessAddress2 { get; set; }
+        public string BusinessCity { get; set; }
+        public string BusinessPostCode { get; set; }
+        public string BusinessTel { get; set; }
+    }
+}
---
+++ HarrisandSonsContactWeb/HarrisandSonsContactWeb/Models/PersonalContact.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HarrisandSonsContactWeb.Models
+{
+    public class PersonalContact
+    {
+        public int PersonalContactID { get; set; }
+        public string PersonalFame { get; set; }
+        public string PersonalLname { get; set; }
+        public string PersonalEmail { get; set; }
+        public string PersonalAddress1 { get; set; }
+        public string PersonalAddress2 { get; set; }
+        public string PersonalCity { get; set; }
+        public string PersonalPostCode { get; set; }
+        public string PersonalTel { get; set; }
+
+
+    }
+}
